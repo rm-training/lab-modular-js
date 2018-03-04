83aa02da520c5a75f985b5bd4659d7b931bed147
Ryan Morris <mr.morris@gmail.com>
2018-03-04 16:59:14 -0500

    Grunt clean of dist and generated

--- todo1/Gruntfile.js
+++ todo1/Gruntfile.js
@@ -63,7 +63,11 @@ module.exports = function(grunt) {
     			src: ['**/*.html'],
     			dest: 'dist/'
     		},
-	    }
+	    },
+	    clean: {
+		  generated: ['generated'],
+		  dist: ['dist']
+		},
 	});
 
 	grunt.registerTask('sayhi', 'Say hello.', function() {
@@ -74,6 +78,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.registerTask('default', [
     	'sayhi',

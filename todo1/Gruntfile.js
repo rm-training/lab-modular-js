918515071d12bee9e8010c6d3ac43729093d441e
Ryan Morris <mr.morris@gmail.com>
2018-03-04 15:18:20 -0500

    Basic grunt and babel - removing dist

--- todo1/Gruntfile.js
+++ todo1/Gruntfile.js
@@ -1,3 +1,33 @@
 module.exports = function(grunt) {
 
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+	    babel: {
+	        options: {
+	            sourceMap: true
+	        },
+	        dist: {
+	        	// concat to one file
+	            // files: [{
+	            //     "src": ["public/js/**/*.js"],
+	            //     "dest": ["public/dist/all.js"],
+	            // }],
+            	// dynamic mapping to get it to expand
+	            files: [{
+	                expand: true,
+	                cwd: 'public/js/',
+	                src: ['**/*.js'],
+	                dest: 'public/dist/js/'
+	            }]
+	        }
+	    }
+	});
+
+	grunt.registerTask('sayhi', 'Say hello.', function() {
+		grunt.log.write('Hello!').ok();
+	});
+
+    grunt.loadNpmTasks('grunt-babel');
+
+    grunt.registerTask("default", ["sayhi", "babel"]);
 };

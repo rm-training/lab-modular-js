eebe5e30d0934fd701061b43c37eac18a7baedf8
Ryan Morris <mr.morris@gmail.com>
2018-03-10 10:15:07 -0500

    Test Flow app with IIFE

--- test-flow/public/js/testfile.js
+++ test-flow/public/js/testfile.js
@@ -1,9 +1,19 @@
-var a = 1;
-console.group('Test file 1!');
-console.log('I am test file 1');
-console.log('I have var a:', a);
+(function() {
+	let a = 1;
+	console.group('Test file 1!');
+	console.log('I am test file 1');
+	console.log('I have var a:', a);
 
-a++;
+	a++;
 
-console.log('I changed a to', a);
-console.groupEnd();
+	console.log('I changed a to', a);
+
+	// this assert will fail depending on the order of script load...
+	setTimeout(function(){
+		console.assert(a===2, 'Someone changed a and it wasn\'t me!');
+	}, 1000);
+
+	mod2.init();
+
+	console.groupEnd();
+})();
--- test-flow/public/js/testfile2.js
+++ test-flow/public/js/testfile2.js
@@ -1,11 +1,25 @@
-var a = 1;
-var b = 2;
-console.group('Test file 2');
-console.log('I am test file 1');
-console.log('I have var a:', a);
-console.log('And I have var b:', b);
+const mod2 = (function() {
+	let a = 10;
+	let b = 20;
+	console.group('Test file 2');
+	console.log('I am test file 1');
+	console.log('I have var a:', a);
+	console.log('And I have var b:', b);
 
-a = a + b;
+	a = a + b;
 
-console.log('I changed a to', a);
-console.groupEnd();
+	console.log('I changed a to', a);
+	console.assert(a===30, 'Someone changed a and it wasn\'t me!');
+	console.groupEnd();
+
+	function reload() {
+		console.log('Reloading from module 2', a, b);
+	}
+
+	return {
+		init: function() {
+			console.log('Initializing from module 2');
+		},
+		reload: reload
+	};
+})();

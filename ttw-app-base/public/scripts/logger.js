6a533be5e8ef6ad81ca41f9c3efd09ceb163ce7f
Ryan Morris <mr.morris@gmail.com>
2018-03-16 14:25:23 -0400

    Timer App with Grunt and using IIFE Modules

--- ttw-app-base/public/scripts/logger.js
+++ ttw-app-base/public/scripts/logger.js
@@ -1,26 +1,37 @@
-/**
- * @param  {...array}
- */
-function logger(...args) {
-  console.log(args);
-}
+const logger = (function() {
 
-/**
- * @param  {...array}
- */
-function logFormatted(...args) {
-  console.group('Logger here...');
-  console.log(args);
-  console.groupEnd();
-}
+	/**
+	 * @param  {...array}
+	 */
+	function logger(...args) {
+	  console.log(args);
+	}
 
-/**
- * @param  {...array}
- */
-function logGrouped(...args) {
-  console.group('Logger here...');
-  args.forEach(function(el) {
-    logger(el);
-  });
-  console.groupEnd();
-}
+	/**
+	 * @param  {...array}
+	 */
+	function logFormatted(...args) {
+	  console.group('Logger here...');
+	  console.log(args);
+	  console.groupEnd();
+	}
+
+	/**
+	 * @param  {...array}
+	 */
+	function logGrouped(...args) {
+	  console.group('Logger here...');
+	  args.forEach(function(el) {
+	    logger(el);
+	  });
+	  console.groupEnd();
+	}
+
+	return {
+		logger,
+		logFormatted,
+		logGrouped
+	};
+
+})();
+

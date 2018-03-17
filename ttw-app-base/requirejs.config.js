0fad30ed528192d54c7f36d149626f331c381673
Ryan Morris <mr.morris@gmail.com>
2018-03-17 10:00:39 -0400

    Timer app - Set up basic requirejs optimization (vanilla)

--- ttw-app-base/requirejs.config.js
+++ ttw-app-base/requirejs.config.js
@@ -1,10 +1,15 @@
 ({
   // start to look for modules by id from here
   // all path lookups will be relative
-  baseUrl: "public/scripts",
+  baseUrl: 'public/scripts',
   paths: {
     // unless a module id begins with vendor, then look here...
     vendor: '../vendor'
   },
-  waitSeconds: 15
+  // tell RequireJS not to optimize/uglify because we're using ES6 code
+  optimize: 'none',
+  // Entry point for the modules
+  name: 'app',
+  // output file
+  out: 'generated/scripts/r-build.js'
 });

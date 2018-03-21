a19138f19ae477f729e3b4df67db398089ac2119
Ryan Morris <mr.morris@gmail.com>
2018-03-21 08:32:22 -0700

    In Class - In progress. Just started Grunt set up and stopped at Babel

---
+++ ttw-app-base/Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		jshint: {
+			options: {
+				esversion: 6
+			},
+			src: ["Gruntfile.js", "public/scripts/*.js"]
+		},
+		babel: {
+			options: {
+				sourceMap: true
+			},
+			generated: {
+				files: [
+					{
+						expand: true, // enables most dyn. stuff
+						cwd: "public/scripts/", // must be a string!
+						src: ["*.js"],
+						dest: "generated/scripts"
+					}
+				]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks('grunt-babel');
+
+	grunt.registerTask("default", ["jshint"]);
+};
